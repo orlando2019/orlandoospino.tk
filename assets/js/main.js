e518a7d38b3a2ce9b033fcf50ffa345bdd1a98b8
orlando1979 <orlando1979@outlook.com>
2020-09-03 01:12:10 -0500

    Termino plantilla y corrijo error en el archivo js, sobre scroll y activate.

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,18 +26,22 @@ const sections = document.querySelectorAll("section[id]");
 
 window.addEventListener("scroll", scrollActive);
 
-function scrollAtive() {
+function scrollActive() {
     const scrollY = window.pageYOffset;
 
-    sections.forEach((current) => {
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute("id");
 
-        if (scrollY > sectionTop && scrollY <= sectionTop +sectionHeight){
-            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active')
-        }else{
-            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active')
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector('.nav__menu a[href*=' + sectionId + ']')
+                .classList.add('active');
+        } else {
+            document
+                .querySelector('.nav__menu a[href*=' + sectionId + ']')
+                .classList.remove('active');
         }
     });
 }
